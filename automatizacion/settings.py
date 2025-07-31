bc2d97be637f570eb3a98ec5215574b96dd3743e
EstbanGa <estban1503@gmail.com>
2025-07-31 12:47:10 -0500

    arreglo en host permitido

--- automatizacion/settings.py
+++ automatizacion/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-k31fa5wajk7n(6%s1x!a)y+zm-s@$6nfds4=)yi=f1k%g3z!kx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
-
+ALLOWED_HOSTS = ['financialsystem-j93c.onrender.com']
 
 # Application definition
 
