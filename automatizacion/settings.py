e6de904b821df7bcc0147bc5b43fd4583a178008
EstbanGa <estban1503@gmail.com>
2025-07-31 12:38:20 -0500

    Arreglo en migraciones y gitignore

--- automatizacion/settings.py
+++ automatizacion/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
