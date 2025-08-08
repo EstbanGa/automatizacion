3b4db0d55ff01fa6824b4c9922022acb47c56d7e
EstbanGa <estban1503@gmail.com>
2025-08-08 13:25:17 -0500

    Agregar Edad actual de la maquina, mejorar un poco el front de la base y listar clientes, rankink de 1 a 5

--- core/forms.py
+++ core/forms.py
@@ -20,7 +20,7 @@ class MaquinaForm(forms.ModelForm):
             'tipo': 'Tipo de máquina (Challenger/Defender)',
             'numero_serie': 'Número de serie',
             'equipment_number': 'Número de equipo',
-            'criticality_ranking': 'Ranking de criticidad (1-10)',
+            'criticality_ranking': 'Ranking de criticidad (1-5)',
             'availability': 'Disponibilidad (%)',
             'date_in_service': 'Fecha de puesta en servicio',
             'machine_age': 'Edad de la máquina (años)',
@@ -99,7 +99,7 @@ class MaquinaForm(forms.ModelForm):
             if field_name == 'criticality_ranking':
                 field.widget.attrs.update({
                     'min': '1',
-                    'max': '10',
+                    'max': '5',
                     'step': '0.1'
                 })
             
